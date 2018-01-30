b4e7323e9783797eb2c25b1a9f7e09a737dd314a
kjordan88 <kristy.jordan88@gmail.com>
2018-01-30 12:45:50 -0500

    fixes breaking submit issues

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,33 +1,38 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  skip_before_action :verify_authenticity_token
 
   # GET /users/new
   def new
-    @user = User.new
+    if params['current_partial']
+      render :new
+    else
+      @user = User.new
+    end
   end
 
   # POST /users
   # POST /users.json
-  def create
-    @user = User.new(user_params)
 
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render :show, status: :created, location: @user }
-      else
-        format.html { render :new }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+  def create
+    user_params = {
+      'first_name': cookies['first_name'],
+      'last_name': cookies['last_name'],
+      'email': cookies['email'],
+      'age': cookies['age'],
+      'weight': cookies['weight'],
+      'height_feet': cookies['height_feet'],
+      'height_inches': cookies['height_inches'],
+      'favorite_color': cookies['favorite_color']
+    }
+    @user = User.new(user_params)
+    if @user.save
+      render :partial => 'finish', notice: 'USER CREATED!'
+    else
+      render :new, notice: 'USER NOT CREATED!'
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_user
-      @user = User.find(params[:id])
-    end
-
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
       params.permit(:first_name, :last_name, :email, :age, :height_feet, :height_inches, :weight, :favorite_color)
